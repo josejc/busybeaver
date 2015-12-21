8379090a51784ce179b9eb43b4fca9da8da10788
José J. Cabezas <josej.cabezas@gmail.com>
2015-12-21 22:22:57 +0100

    Array of string for the states of busybeaver

--- busybeaver.go
+++ busybeaver.go
@@ -9,36 +9,42 @@ import (
 const lenght = 25
 
 // Structs
-// -State is equivalent a 'ncard' of compterphile
+// -State is equivalent a 'ncard' of computerphile
 type state struct {
-	c_wr byte	// new character to write in the tape
-	shift byte	// left or right
-	nstate int	// next state
+	c_wr   byte // new character to write in the tape
+	shift  byte // left or right
+	nstate int  // next state
 }
 
-
 // Create new busybeaver (turing machine) whith selection of one predefined
-func newbb(s int) []state {
+func newbb(s int) [][2]string {
+	var bb [][2]string
 	switch s {
-		case 0:
-			return make([]state, 5)			
-		default:	
+	case 0:
+		// tm1a - loops off to the right
+		bb = [][2]string{{"000", "000"}, {"111", "111"}}
+	default:
+		bb = make([][2]string, 0)
 	}
-	return make([]state, 4)
+	return bb
 }
 
 // Print the definition os busybeaver, function only for evaluation
-func printbb(bb []state) {
+func printbb(bb [][2]string) {
 	fmt.Println("lenght: ", len(bb))
-	fmt.Println("capacity: ", cap(bb))	
+	fmt.Println("capacity: ", cap(bb))
+	fmt.Println("bb: :", bb)
 	fmt.Println("---")
+
 }
 
+// Print tape
 func printt(t []byte) {
 	fmt.Println("Tape lenght: ", len(t))
-	for i := 0; i < len(t); i++ {
-		fmt.Print(i,",",t[i], "-")
-	}
+	fmt.Println(t)
+	//	for i := 0; i < len(t); i++ {
+	//		fmt.Print(i, ",", t[i], "-")
+	//	}
 	fmt.Println("...")
 }
 
@@ -51,7 +57,7 @@ func check(e error) {
 
 func main() {
 	var tape []byte
-	var bb []state
+	var bb [][2]string
 
 	tape = make([]byte, lenght)
 	printt(tape)
